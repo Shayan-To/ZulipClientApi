ae09d2650c2685c0c1e7c502087dac2b8af6b9c3
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-04-04 07:49:46 +0430

    Add VTuple and Extensions.
    VTuple is a simple value-type tuple.
    Extensions class is a container for extension methods.
    Also add Extensions.BinarySearch.

---
+++ Helpers/Extensions.vb
@@ -0,0 +1,73 @@
+﻿Friend Module Extensions
+
+    Private Function LeastPowerOfTwoOnMin(ByVal Min As Integer) As Integer
+        If Min < 1 Then
+            Return 1
+        End If
+
+        ' If Min is a power of two, we should return Min, otherwise, Min * 2
+        Dim T = (Min - 1) And Min
+        If T = 0 Then
+            Return Min
+        End If
+        Min = T
+
+        Do
+            T = (Min - 1) And Min
+            If T = 0 Then
+                Return Min << 1
+            End If
+            Min = T
+        Loop
+    End Function
+
+    ''' <summary>
+    ''' Gets the interval in which Value resides in inside a sorted list.
+    ''' </summary>
+    ''' <param name="Value">The value to look for.</param>
+    ''' <returns>
+    ''' The tuple (start index, length).
+    ''' Start index being the index of fist occurrance of Value, and length being the count of its occurrances.
+    ''' If no occurrance of Value has been found, start index will be at the first element larger than Value.
+    ''' </returns>
+    <Extension()>
+    Public Function BinarySearch(Of T)(ByVal Self As T(), ByVal Value As T, ByVal Comp As Comparison(Of T)) As VTuple(Of Integer, Integer)
+        Dim Count = LeastPowerOfTwoOnMin(Self.Length + 1) \ 2
+        Dim Offset1 = -1
+
+        Do While Count > 0
+            If Offset1 + Count < Self.Length Then
+                Dim C = Comp.Invoke(Self(Offset1 + Count), Value)
+                If C < 0 Then
+                    Offset1 += Count
+                ElseIf C = 0 Then
+                    Exit Do
+                End If
+            End If
+            Count \= 2
+        Loop
+
+        Dim Offset2 = Offset1
+        If Count > 0 Then
+            ' This should have been done in the ElseIf block in the previous loop before the Exit statement.
+            Offset2 += Count
+
+            Do While Count > 1
+                Count \= 2
+                If Offset1 + Count < Self.Length Then
+                    If Comp.Invoke(Self(Offset1 + Count), Value) < 0 Then
+                        Offset1 += Count
+                    End If
+                End If
+                If Offset2 + Count < Self.Length Then
+                    If Comp.Invoke(Self(Offset2 + Count), Value) <= 0 Then
+                        Offset2 += Count
+                    End If
+                End If
+            Loop
+        End If
+
+        Return VTuple.Create(Offset1 + 1, Offset2 - Offset1)
+    End Function
+
+End Module
---
+++ Helpers/VTuple.vb
@@ -0,0 +1,92 @@
+﻿Public NotInheritable Class VTuple
+
+    Private Sub New()
+        Throw New NotSupportedException()
+    End Sub
+
+    Public Shared Function Create(Of T1, T2)(ByVal Item1 As T1, ByVal Item2 As T2) As VTuple(Of T1, T2)
+        Return New VTuple(Of T1, T2)(Item1, Item2)
+    End Function
+
+    Public Shared Function Create(Of T1, T2, T3)(ByVal Item1 As T1, ByVal Item2 As T2, ByVal Item3 As T3) As VTuple(Of T1, T2, T3)
+        Return New VTuple(Of T1, T2, T3)(Item1, Item2, Item3)
+    End Function
+
+End Class
+
+Public Structure VTuple(Of T1, T2)
+
+    Public Sub New(ByVal Item1 As T1, ByVal Item2 As T2)
+        Me._Item1 = Item1
+        Me._Item2 = Item2
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return String.Concat("VTuple{", Me._Item1, ", ", Me._Item2, "}")
+    End Function
+
+#Region "Item1 Property"
+    Private ReadOnly _Item1 As T1
+
+    Public ReadOnly Property Item1 As T1
+        Get
+            Return Me._Item1
+        End Get
+    End Property
+#End Region
+
+#Region "Item2 Property"
+    Private ReadOnly _Item2 As T2
+
+    Public ReadOnly Property Item2 As T2
+        Get
+            Return Me._Item2
+        End Get
+    End Property
+#End Region
+
+End Structure
+
+Public Structure VTuple(Of T1, T2, T3)
+
+    Public Sub New(ByVal Item1 As T1, ByVal Item2 As T2, ByVal Item3 As T3)
+        Me._Item1 = Item1
+        Me._Item2 = Item2
+        Me._Item3 = Item3
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return String.Concat("VTuple{", Me._Item1, ", ", Me._Item2, ", ", Me._Item3, "}")
+    End Function
+
+#Region "Item1 Property"
+    Private ReadOnly _Item1 As T1
+
+    Public ReadOnly Property Item1 As T1
+        Get
+            Return Me._Item1
+        End Get
+    End Property
+#End Region
+
+#Region "Item2 Property"
+    Private ReadOnly _Item2 As T2
+
+    Public ReadOnly Property Item2 As T2
+        Get
+            Return Me._Item2
+        End Get
+    End Property
+#End Region
+
+#Region "Item3 Property"
+    Private ReadOnly _Item3 As T3
+
+    Public ReadOnly Property Item3 As T3
+        Get
+            Return Me._Item3
+        End Get
+    End Property
+#End Region
+
+End Structure
