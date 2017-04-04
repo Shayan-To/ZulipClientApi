f5becc09f716abde9ad59c30246ea1c5368fbc6c
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-04-04 07:14:44 +0430

    Add Assert and AssertionException.
    Just like Verify, for assertion.

---
+++ Helpers/Assert.vb
@@ -0,0 +1,22 @@
+﻿Public Class Assert
+
+    <DebuggerHidden()>
+    Public Shared Sub [True](ByVal T As Boolean, Optional ByVal Message As String = Nothing)
+        If Not T Then
+            Fail(Message)
+        End If
+    End Sub
+
+    <DebuggerHidden()>
+    Public Shared Sub [False](ByVal T As Boolean, Optional ByVal Message As String = Nothing)
+        If T Then
+            Fail(Message)
+        End If
+    End Sub
+
+    <DebuggerHidden()>
+    Public Shared Sub Fail(Optional ByVal Message As String = Nothing)
+        Throw New AssertionException(Message)
+    End Sub
+
+End Class
---
+++ Helpers/AssertionException.vb
@@ -0,0 +1,16 @@
+﻿Public Class AssertionException
+    Inherits Exception
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ByVal Message As String)
+        MyBase.New(Message)
+    End Sub
+
+    Public Sub New(ByVal Message As String, ByVal InnerException As Exception)
+        MyBase.New(Message, InnerException)
+    End Sub
+
+End Class
